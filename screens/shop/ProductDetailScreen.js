f92b6e74ef7988c4539e166308ff90f89ae1d3ac
Bill Buchanan <bbuchanan@gmail.com>
2019-10-08 09:32:11 -0400

    164. Adding items to the cart

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -8,11 +8,13 @@ import {
   StyleSheet
 } from "react-native";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import * as cartActions from "../../store/actions/cart";
 
 import Colors from "../../constants/Colors";
 
 const ProductDetailScreen = props => {
+  const dispatch = useDispatch();
   const productId = props.navigation.getParam("productId");
   const selectedProduct = useSelector(state =>
     state.products.availableProducts.find(p => p.id === productId)
@@ -22,7 +24,13 @@ const ProductDetailScreen = props => {
     <ScrollView>
       <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
       <View style={styles.action}>
-        <Button color={Colors.primary} title="Add to Cart" onPress={() => {}} />
+        <Button
+          color={Colors.primary}
+          title="Add to Cart"
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct));
+          }}
+        />
       </View>
       <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
       <Text style={styles.description}>{selectedProduct.description}</Text>

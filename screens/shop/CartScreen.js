4aee2c3eecd796a5ea4d1a8c35ed74af279b2d0c
Bill Buchanan <bbuchanan@gmail.com>
2019-10-08 11:13:56 -0400

    166. Outputting Cart Items

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { useSelector } from "react-redux";
 import Colors from "../../constants/Colors";
+import CartItem from "../../components/shop/CartItem";
 
 const CartScreen = props => {
   const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -32,9 +33,18 @@ const CartScreen = props => {
           disabled={cartItems.length === 0}
         />
       </View>
-      <View>
-        <Text>CART ITEMS</Text>
-      </View>
+      <FlatList
+        data={cartItems}
+        key={i => i.productId}
+        renderItem={itemData => (
+          <CartItem
+            quantity={itemData.item.quantity}
+            title={itemData.item.productTitle}
+            amount={itemData.item.sum}
+            onRemove={() => {}}
+          />
+        )}
+      />
     </View>
   );
 };

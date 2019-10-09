2c3a6618ef9b4dd8dd2ea06b84c8534cc8e56717
Bill Buchanan <bbuchanan@gmail.com>
2019-10-09 15:47:09 -0400

    197. Fetching products from the server

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,7 +1,36 @@
+import Product from "../../models/product";
+
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const SET_PRODUCTS = "SET_PRODUCTS";
 
+export const fetchProducts = () => {
+  return async dispatch => {
+    const response = await fetch(
+      "https://rn-shop-a3001.firebaseio.com/products.json"
+    );
+
+    const resData = await response.json();
+    const loadedProducts = [];
+
+    for (const key in resData) {
+      const item = resData[key];
+      loadedProducts.push(
+        new Product(
+          key,
+          "u1",
+          item.title,
+          item.imageUrl,
+          item.description,
+          item.price
+        )
+      );
+    }
+
+    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+  };
+};
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
